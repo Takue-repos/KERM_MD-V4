bd2e8797fe02e8d0e5cbb17e4de4d6e227f8c484
Takue-repos <tdzwa61@gmail.com>
2025-01-02 19:56:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ENTER YOUR SESSION HERE",
+SESSION_ID: process.env.SESSION_ID || "fdQiQYKQ#l6ic9J3tb05u7gNID_-QYaGUOg_4fPWEWbvOHXam3m0",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
